dfd7326cc68ed00ea39ea9b61f635b6474490cdb
hannbusann <1617126946@qq.com>
2018-10-12 21:22:44 +0800

    Simplified polynomial

--- Polynomial/Polynomial.hpp
+++ Polynomial/Polynomial.hpp
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2015, Jonathan Ventura
+ * Modified by fw, 2018.10
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
@@ -10,7 +11,12 @@
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-
+/**
+ * 这个源码连同PolynomialInternal.hpp一起可以独立成为一个工具模块
+ * 可以提供多项式的 加、减、乘、带入求值、求根 五种运算
+ * 具体使用方法可以参照 http://github.com/hannbusann/Polynomial_math/test 中的例程
+ * 
+ */
 #ifndef POLYNOMIAL_HPP
 #define POLYNOMIAL_HPP
 
@@ -36,18 +42,19 @@ namespace polynomial
      *
      * Note that the static and dynamic versions should not be mixed, i.e. do not add a dynamic polynomial to a static one.
      */
-    template<int deg>
+
+    template<int deg>   // deg equals to degree of your polynomial
     class Polynomial
     {
         Eigen::Matrix<double,deg+1,1> coef;
     public:
-        Polynomial()
+        Polynomial()   //创建deg阶的系数都为0的多项式
         : coef( Eigen::Matrix<double,1,deg+1>::Zero() )
         {
             
         }
         
-        Polynomial( const Eigen::Matrix<double,deg+1,1> &coefin )
+        Polynomial( const Eigen::Matrix<double,deg+1,1> &coefin ) //创建有值的
         : coef( coefin )
         {
 
@@ -96,6 +103,7 @@ namespace polynomial
         template<int degin>
         Polynomial<degin+deg> operator*(const Polynomial<degin> &poly) const
         {
+            std::cout << "Degree elevation happens!" << std::endl;
             Polynomial<degin+deg> p;
             Internal::PolyConv<deg,degin>::compute(p.coefficients(),coef,poly.coefficients());
             return p;
@@ -103,6 +111,7 @@ namespace polynomial
         
         Polynomial<deg> operator*(const double c) const
         {
+            std::cout << "Degree still!" << std::endl;
             return Polynomial<deg>(coef*c);
         }
         
@@ -111,18 +120,11 @@ namespace polynomial
             return Internal::PolyVal<deg>::compute(coef,x);
         }
         
-        void realRoots(std::vector<double> &roots) const
-        {
-            if ( coef[0] == 0 )
-            {
-                Internal::RootFinder<deg-1>::compute(coef.tail(deg),roots);
-            } else {
-                Internal::RootFinder<deg>::compute(coef,roots);
-            }
-        }
         
-        void realRootsSturm(const double lb, const double ub, std::vector<double> &roots) const
+        void realRootsSturm( std::vector<double> &roots) const
         {
+            double lb,ub;
+            rootBounds(lb,ub);
             if ( coef[0] == 0 )
             {
                 Internal::SturmRootFinder<deg-1> sturm( coef.tail(deg) );
@@ -133,116 +135,10 @@ namespace polynomial
             }
         }
         
-        void rootBounds( double &lb, double &ub )
+        void rootBounds( double &lb, double &ub ) const
         {
             Eigen::Matrix<double,deg,1> mycoef = coef.tail(deg).array().abs();
             mycoef /= fabs(coef(0));
-            mycoef(0) += 1.;
-            ub = mycoef.maxCoeff();
-            lb = -ub;
-        }
-    };
-    
-    template <>
-    class Polynomial<Eigen::Dynamic>
-    {
-        Eigen::VectorXd coef;
-    public:
-        Polynomial(const int deg)
-        : coef( Eigen::VectorXd::Zero(deg+1) )
-        {
-            
-        }
-        
-        Polynomial( const Eigen::VectorXd &coefin)
-        : coef( coefin )
-        {
-
-        }
-        
-        Polynomial(const Polynomial<Eigen::Dynamic> &polyin)
-        : coef( polyin.coef )
-        {
-
-        }
-        
-        const Eigen::VectorXd &coefficients() const
-        {
-            return coef;
-        }
-        
-        Eigen::VectorXd &coefficients()
-        {
-            return coef;
-        }
-        
-        Polynomial<Eigen::Dynamic> operator+(const Polynomial<Eigen::Dynamic> &poly) const
-        {
-            int deg = coef.rows()-1;
-            int degin = poly.coef.rows()-1;
-            Polynomial<Eigen::Dynamic> p( std::max(deg,degin) );
-            p.coef.tail(degin+1) = poly.coef;
-            p.coef.tail(deg+1) += coef;
-            return p;
-        }
-
-        Polynomial<Eigen::Dynamic> operator-(const Polynomial<Eigen::Dynamic> &poly) const
-        {
-            int deg = coef.rows()-1;
-            int degin = poly.coef.rows()-1;
-            Polynomial<Eigen::Dynamic> p( std::max(deg,degin) );
-            p.coef.tail(deg+1) = coef;
-            p.coef.tail(degin+1) -= poly.coef;
-            return p;
-        }
-        
-        Polynomial<Eigen::Dynamic> operator*(const Polynomial<Eigen::Dynamic> &poly) const
-        {
-            int deg = coef.rows()-1;
-            int degin = poly.coef.rows()-1;
-            Polynomial<Eigen::Dynamic> p( deg+degin );
-            Internal::PolyConv<Eigen::Dynamic,Eigen::Dynamic>::compute(p.coef,coef,poly.coef);
-            return p;
-        }
-        
-        Polynomial<Eigen::Dynamic> operator*(const double c) const
-        {
-            return Polynomial<Eigen::Dynamic>(coef*c);
-        }
-        
-        double eval(double x) const
-        {
-            return Internal::PolyVal<Eigen::Dynamic>::compute(coef,x);
-        }
-        
-        void realRoots(std::vector<double> &roots) const
-        {
-            if ( coef[0] == 0 )
-            {
-                int deg = coef.rows()-1;
-                Internal::RootFinder<Eigen::Dynamic>::compute(coef.tail(deg),roots);
-            } else {
-                Internal::RootFinder<Eigen::Dynamic>::compute(coef,roots);
-            }
-        }
-        
-        void realRootsSturm(const double lb, const double ub, std::vector<double> &roots) const
-        {
-            if ( coef[0] == 0 )
-            {
-                int deg = coef.rows()-1;
-                Internal::SturmRootFinder<Eigen::Dynamic> sturm( coef.tail(deg) );
-                sturm.realRoots( lb, ub, roots );
-            } else {
-                Internal::SturmRootFinder<Eigen::Dynamic> sturm( coef );
-                sturm.realRoots( lb, ub, roots );
-            }
-        }
-        
-        void rootBounds( double &lb, double &ub )
-        {
-            int deg = coef.rows()-1;
-            Eigen::VectorXd mycoef = coef.tail(deg).array().abs()/fabs(coef(0));
             mycoef(0) += 1.;
             ub = mycoef.maxCoeff();
             lb = -ub;
